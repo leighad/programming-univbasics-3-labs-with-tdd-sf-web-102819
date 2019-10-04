56a4453cb25ab38e5c54eb3356410113292b29f9
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-10-04 02:01:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number = 26
 second_number = 27
 sum = first_number + second_number
+difference = first_number - second_number
+#sum = first_number + second_number
+#sum = first_number + second_number
